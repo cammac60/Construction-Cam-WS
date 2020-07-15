5fab0b15ea473df8d40c8e1aad4493058ade5a10
Cameron MacRae <cam.mac.cm@gmail.com>
2020-07-15 17:15:57 -0600

    Add imports and setup methods

--- scraper.py
+++ scraper.py
@@ -2,7 +2,16 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-from bs4 import BeautifulSoup
 import pandas as pd
 import re
 import os
+
+base_url = "https://forum.skyscraperpage.com/showthread.php?t=227316&page=1"
+
+driver = webdriver.Firefox()
+driver.get(base_url)
+driver.implicitly_wait(100)
+
+num_links = len(driver.find_elements_by_class_name('pagenavi'))
+
+print(num_links)
