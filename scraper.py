14cb26497e373a58c0afe61f6ce506165ca4cbdc
Cameron MacRae <cam.mac.cm@gmail.com>
2020-07-15 10:40:47 -0600

    Initial commit

---
+++ scraper.py
@@ -0,0 +1,8 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+import os
