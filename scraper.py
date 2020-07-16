c88d494424d87bbd1f4cff00446599eb9aebdde1
Cameron MacRae <cam.mac.cm@gmail.com>
2020-07-16 15:11:48 -0600

    Finish for loop

--- scraper.py
+++ scraper.py
@@ -28,18 +28,24 @@ def collect_comments():
     return comments
 
 def match_text(text):
-    pass
+    search_terms = ["Camera", "camera", "Cameras", "cameras", "construction camera", "construction cameras", "Construction camera", "Construction cameras", "construction cam", "Construction cam", "construction cams", "Construction cams", "Construction Cam", "Construction Cams", "Zeppelin"]
+    return any(x in text for x in search_terms)
 
 def filter_comments(comment_array):
     for i in range(len(comment_array)):
         if match_text(comment_array[i]):
-            pass
+            urls = re.findall(r'(https?://[^\s]+)', comment_array[i])
+            if len(urls) >= 1:
+                return urls
 
 for i in range(0, int(max_page_num) + 1):
     driver.implicitly_wait(5)
-    next_button = num_buttons.find_elements_by_xpath(".//a")[8]
     comments = collect_comments()
-    if i == 0:
-        print(comments)
+    url_list = filter_comments(comments)
+    if url_list:
+        results = results + url_list
+    driver.get(base_url + str(i + 1))
+
+print(results)
 
 driver.close()
