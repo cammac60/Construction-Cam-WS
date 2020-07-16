8812a0e5bb0bccde266d6f821624eb688a33f9b5
Cameron MacRae <cam.mac.cm@gmail.com>
2020-07-16 16:01:16 -0600

    Add notes for bug fixes

--- scraper.py
+++ scraper.py
@@ -6,6 +6,12 @@ import pandas as pd
 import re
 import os
 
+# // TODO //
+
+# Refactor to allow the collection on the innerhtml of the comments.
+# Refactor so that final result is the full url from the href
+# See lines 33, 44, and 45
+
 base_url = "https://forum.skyscraperpage.com/showthread.php?t=227316&page="
 
 driver = webdriver.Firefox()
@@ -24,6 +30,7 @@ results = []
 def collect_comments():
     comments = driver.find_elements_by_css_selector("div[id*='post_message']")
     for i in range(len(comments)):
+        # As noted on line 38, we need to get href which requires this to be changed to innerhtml
         comments[i] = comments[i].get_attribute('innerText')
     return comments
 
@@ -31,17 +38,19 @@ def match_text(text):
     search_terms = ["Camera", "camera", "Cameras", "cameras", "construction camera", "construction cameras", "Construction camera", "Construction cameras", "construction cam", "Construction cam", "construction cams", "Construction cams", "Construction Cam", "Construction Cams"]
     return any(x in text for x in search_terms)
 
-def filter_comments(comment_array):
+def filter_comments(comment_array, index):
     for i in range(len(comment_array)):
         if match_text(comment_array[i]):
-            urls = re.findall(r'(https?://[^\s]+)', comment_array[i])
+            # Now passing in index to allow us to collect the page number where the url was found. This will allow for easy collection of full urls even if the collected url is corrupted.
+            # Many of the urls are collapsed, we need to find the href of the comments to get the full link.
+            # urls = re.findall(r'(https?://[^\s]+)', comment_array[i])
             if len(urls) >= 1:
                 return urls
 
 for i in range(0, int(max_page_num) + 1):
     driver.implicitly_wait(5)
     comments = collect_comments()
-    url_list = filter_comments(comments)
+    url_list = filter_comments(comments, i)
     if url_list:
         results = results + url_list
     driver.get(base_url + str(i + 1))
