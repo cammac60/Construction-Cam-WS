55561a99376f9da4c74f8a46e2c82f124bd26cf4
Cameron MacRae <cam.mac.cm@gmail.com>
2020-07-16 10:38:21 -0600

    Add max time print statement

--- scraper.py
+++ scraper.py
@@ -15,8 +15,15 @@ driver.implicitly_wait(10)
 link_wrapper = driver.find_elements_by_class_name('pagenavi')[0]
 num_buttons = link_wrapper.find_elements_by_xpath(".//div")[1]
 max_page_num = num_buttons.find_elements_by_xpath(".//a")[7].get_attribute('innerText')
+max_expected_wait = int((int(max_page_num) * 5) / 60)
+
+print("Maximum wait time: " + str(max_expected_wait) + " minutes")
+
+results = []
 
 for i in range(0, int(max_page_num) + 1):
-    pass
+    driver.implicitly_wait(5)
+    next_button = num_buttons.find_elements_by_xpath(".//a")[8]
+
 
 driver.close()
