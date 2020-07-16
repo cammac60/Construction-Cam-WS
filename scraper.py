8b904fc37630c803e7ff66a52b2d2200017d84e3
Cameron MacRae <cam.mac.cm@gmail.com>
2020-07-16 11:05:39 -0600

    Start helper methods

--- scraper.py
+++ scraper.py
@@ -21,9 +21,25 @@ print("Maximum wait time: " + str(max_expected_wait) + " minutes")
 
 results = []
 
+def collect_comments():
+    comments = driver.find_elements_by_css_selector("div[id*='post_message']")
+    for i in range(len(comments)):
+        comments[i] = comments[i].get_attribute('innerText')
+    return comments
+
+def match_text(text):
+    pass
+
+def filter_comments(comment_array):
+    for i in range(len(comment_array)):
+        if match_text(comment_array[i]):
+            pass
+
 for i in range(0, int(max_page_num) + 1):
     driver.implicitly_wait(5)
     next_button = num_buttons.find_elements_by_xpath(".//a")[8]
-
+    comments = collect_comments()
+    if i == 0:
+        print(comments)
 
 driver.close()
